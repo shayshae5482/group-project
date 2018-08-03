da2434853b650b4a938644200f650d969f12be41
shayshae5482 <shayshae5482@gmail.com>
2018-08-02 21:01:17 -0500

    initial hamburger--can edit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,6 +53,16 @@ function initMap() {
     });
 }
 
+//Hamburger in nav menu
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger--spring-r");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+  
+});
 
 var cuisineChoice;
 var zomatoCityID;
